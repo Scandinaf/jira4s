edb59e8c8053872e47eb0fcceb2fb08735944ab3
scandinaf_ <sbarouski@evolutiongaming.com>
2020-01-28 18:56:00 +0300

    fix bug. Changelog use JiraUser instead of Author.

--- src/main/scala/com/allantl/jira4s/v2/domain/Changelog.scala
+++ src/main/scala/com/allantl/jira4s/v2/domain/Changelog.scala
@@ -10,7 +10,7 @@ object Changelog {
   implicit val encoder: Encoder[Changelog] = deriveEncoder
 }
 
-case class History(id: String, author: JiraUser, created: String, items: List[HistoryItem])
+case class History(id: String, author: Author, created: String, items: List[HistoryItem])
 
 object History {
   implicit val decoder: Decoder[History] = deriveDecoder
@@ -18,50 +18,44 @@ object History {
 }
 
 case class HistoryItem(
-    field: String,
-    fieldType: String,
-    fieldId: String,
-    from: Option[String],
-    fromString: Option[String],
-    to: Option[String],
-    tostring: Option[String]
-)
+                        field: String,
+                        fieldType: String,
+                        fieldId: String,
+                        from: Option[String],
+                        fromString: Option[String],
+                        to: Option[String],
+                        tostring: Option[String]
+                      )
 
 object HistoryItem {
 
-  implicit val encoder: Encoder[HistoryItem] = new Encoder[HistoryItem] {
-    override def apply(hi: HistoryItem): Json =
-      Json.obj(
-        ("field", Json.fromString(hi.field)),
-        ("fieldtype", Json.fromString(hi.fieldType)),
-        ("fieldId", Json.fromString(hi.fieldId)),
-        ("from", hi.from.fold(Json.Null)(Json.fromString)),
-        ("fromString", hi.fromString.fold(Json.Null)(Json.fromString)),
-        ("to", hi.to.fold(Json.Null)(Json.fromString)),
-        ("toString", hi.to.fold(Json.Null)(Json.fromString))
-      )
-  }
+  implicit val encoder: Encoder[HistoryItem] = (hi: HistoryItem) => Json.obj(
+    ("field", Json.fromString(hi.field)),
+    ("fieldtype", Json.fromString(hi.fieldType)),
+    ("fieldId", Json.fromString(hi.fieldId)),
+    ("from", hi.from.fold(Json.Null)(Json.fromString)),
+    ("fromString", hi.fromString.fold(Json.Null)(Json.fromString)),
+    ("to", hi.to.fold(Json.Null)(Json.fromString)),
+    ("toString", hi.to.fold(Json.Null)(Json.fromString))
+  )
 
-  implicit val decoder: Decoder[HistoryItem] = new Decoder[HistoryItem] {
-    override def apply(c: HCursor): Decoder.Result[HistoryItem] =
-      for {
-        field <- c.downField("field").as[String].right
-        fieldType <- c.downField("fieldtype").as[String].right
-        fieldId <- c.downField("fieldId").as[String].right
-        from <- c.downField("from").as[Option[String]].right
-        fromString <- c.downField("fromString").as[Option[String]].right
-        to <- c.downField("to").as[Option[String]].right
-        toString <- c.downField("toString").as[Option[String]].right
-      } yield
-        HistoryItem(
-          field,
-          fieldType,
-          fieldId,
-          from,
-          fromString,
-          to,
-          toString
-        )
-  }
+  implicit val decoder: Decoder[HistoryItem] = (c: HCursor) => for {
+    field <- c.downField("field").as[String].right
+    fieldType <- c.downField("fieldtype").as[String].right
+    fieldId <- c.downField("fieldId").as[String].right
+    from <- c.downField("from").as[Option[String]].right
+    fromString <- c.downField("fromString").as[Option[String]].right
+    to <- c.downField("to").as[Option[String]].right
+    toString <- c.downField("toString").as[Option[String]].right
+  } yield
+    HistoryItem(
+      field,
+      fieldType,
+      fieldId,
+      from,
+      fromString,
+      to,
+      toString
+    )
 
 }
